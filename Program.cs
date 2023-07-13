5407966aab4d5bcad55467b96e717656ce1c4381
Karthick M <karthickmtvm@gmail.com>
2023-07-13 14:11:28 +0530

    commented

--- Program.cs
+++ Program.cs
@@ -11,19 +11,19 @@ namespace Program
     {
         static async Task Main(string[] args)
         {
-            await AsyncBreakfast.Program.display();
-            linq.test_scores.display();
-            linq.test_mul_select.display();
-            linq.test_orderby.display();
-            linq.test_count.display();
-            access.test t = new access.test();
-            t.display();
-            rec.test_person.display();
-            delegates.test_delegate.display();
-            collections.test_collections.display();
-            await asyncExample.x.DisplayCurrentInfoAsync();
-            stringBuilder.builder.display();
-            check.test_check.display();
+            // await AsyncBreakfast.Program.display();
+            // linq.test_scores.display();
+            // linq.test_mul_select.display();
+            // linq.test_orderby.display();
+            // linq.test_count.display();
+            // access.test t = new access.test();
+            // t.display();
+            // rec.test_person.display();
+            // delegates.test_delegate.display();
+            // collections.test_collections.display();
+            // await asyncExample.x.DisplayCurrentInfoAsync();
+            // stringBuilder.builder.display();
+            // check.test_check.display();
         }
     }
 }
